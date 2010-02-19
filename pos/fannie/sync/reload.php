e3d1c451e32cb1ebdff6b01d64561483d1ff27c5
Wedge Co-op IT Department <dba@homer.wedgenet>
2010-02-20 06:19:42 +0800

    Bunch more changes to the sync folder in fannie

--- pos/fannie/sync/reload.php
+++ pos/fannie/sync/reload.php
@@ -1,236 +1,86 @@
 <?php
-/*******************************************************************************
-
-    Copyright 2001, 2004, 2007 Wedge Community Co-op
-
-    This file is part of IS4C.
-
-    IS4C is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    IS4C is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    in the file license.txt along with IS4C; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-*********************************************************************************/
-
-include_once($_SERVER["DOCUMENT_ROOT"]."/define.conf");
-
-function reloadtable($table) {
-
-$aLane = array(
-
-	LANE01,
-	LANE02,
-	LANE03,
-	LANE04,
-	LANE05,
-	LANE06,
-	LANE07,
-	LANE08,
-	LANE09,
-	LANE10,
-	LANE11,
-	LANE12
-
-
-);
-
-$server = "localhost";
-$opDB = "is4c_op";
-$serveruser = $_SESSION["mUser"];
-$serverpass = $_SESSION["mPass"];
-
-$laneuser = $_SESSION["laneUser"];
-$lanepass = $_SESSION["lanePass"];
-
-$file = "/pos/is4c/download/".$table.".out";
-$dump = "select * into outfile '".$file."' from ".$table;
-$load = "load data infile '".$file."' into table is4c_op.".$table;
-
-$is4c_op_truncate = "truncate is4c_op.".$table;
-$opdata_truncate = "truncate opdata.".$table;
-$opdata_insert = "insert into opdata. ".$table." select * from is4c_op.".$table;
-
-echo "<font color='#004080' face=helvetica><b>".$table."</b></font>";
-echo "<p>";
-
-
-$continue = 0;
-
-/* establish connection to server */
-
-if ($s_conn = mysql_connect($server, $serveruser, $serverpass)) {
-	$continue = 1;
-}
-else {
-	echo "<p><font color='#800000' face=helvetica size=-1>Failed to connect to server</font>";
-}
-
-if ($continue == 1) {
-
-	$continue = 0;
-	if (mysql_select_db("is4c_op", $s_conn)) $continue = 1; 
-	else echo "<p><font color='#800000' face=helvetica size=-1>Failed to connect to server database</font>";
-}
-
-if ($continue == 1) {
-
-	$continue = 0;
-	$result = mysql_query("select count(*) from ".$table, $s_conn);
-	$row = mysql_fetch_array($result);
-	$server_num_rows = $row[0];
-	// echo "<p><font color='#004080' face=helvetica size=-1>There are ".$server_num_rows." record(s) on server database</font>";
-	if ($server_num_rows > 10) $continue = 1;
-	else echo "<p><font color='#800000' face=helvetica size=-1>There are only ".$server_num_rows." records on the server.<br>No way</font>";
-
-}
-/*
-if ($continue == 1) {
-
-	$continue = 0;
-	if (file_exists($file)) exec("rm ".$file);
-	if (mysql_query($dump, $s_conn)) $continue = 1;
-	else echo "<p><font color='#800000' face=helvetica size=-1>Failed to download new data from server</font>";
-
-}
-
-
-if ($continue == 1) {
-
-	$continue = 0;
-	if (file_exists($file)) $continue = 1;
-	else echo "<p><font color='#800000' face=helvetica size=-1>Failed to retrieve new data from server</font>";
-
-} 
-*/
-// synchronize lanes
-
-if ($continue == 1) {
-
-//	$continue = 0;
-//	echo "<p><font color='#004080' face=helvetica size=-1>".$server_num_rows." records downloaded from server</font>";
-//	echo "<p>";
+	require_once($_SERVER["DOCUMENT_ROOT"]."/define.conf");
 	
-	$i = 1;
-	foreach ($aLane as $lane) {
-
-		if ($lane && strlen($lane) > 0) {
-		$lane_num = "lane ".$i;
-		$i++;
-		$lane_continue = 0;
-		if ($lane_conn = mysql_connect($lane, $laneuser, $lanepass)) $lane_continue = 1;
-		else echo "<br><font color='#800000' face=helvetica size=-1>Unable to connect to ".$lane_num."</font>";
-
-		if ($lane_continue == 1) {
-			$lane_continue = 0;
-			if (mysql_select_db("opdata", $lane_conn)) $lane_continue = 1;
-			else echo "<br><font color='#800000' face=helvetica size=-1>Unable to select database on ".$lane_num."</font>";
+	if (isset($_REQUEST['t'])) {
+		switch ($_REQUEST['t']) {
+			case 'products':
+				$name='products';
+				$type='Products';
+			break;
+			case 'custdata':
+				$name='custdata';
+				$type='Membership';
+			break;
+			case 'employees':
+				$name='employees';
+				$type='Employees';
+			break;
+			case 'departments':
+				$name='departments';
+				$type='Departments';
+			break;
+			case 'subdepts':
+				$name='subdepts';
+				$type='Subdepartments';
+			break;
+			case 'tenders':
+				$name='tenders';
+				$type='Tenders';
+			break;
+			
+			default:
+				header("Location: index.php");
+			break;
 		}
-
-		if ($lane_continue == 1) {
-			$lane_continue = 0;
-			mysql_query($is4c_op_truncate, $lane_conn);
-
-			if (synctable($table,$serveruser,$opDB,$lane) == 1) {
-
-				$result = mysql_query("select count(*) from is4c_op.".$table, $lane_conn);
-				$row = mysql_fetch_array($result);
-				$lane_num_rows = $row[0];
-				if ($lane_num_rows == $server_num_rows) $lane_continue = 1;
-				else echo "<br><font color='#800000' face=helvetica size=-1>".$lane_num.": Number of records do not match. Synchronization refused</font>";
-
-			}
-			else echo "<br><font color='#800000' face=helvetica size=-1>Unable to load new data onto ".$lane_num."</font>";
-
-		}
-
-		if ($lane_continue == 1) {
-			$lane_continue = 0;
-			if (mysql_query($opdata_truncate, $lane_conn)) {
-				if (mysql_query($opdata_insert, $lane_conn)) {
-					$qresult = mysql_query("select * from ".$table, $lane_conn);
-					$lane_num_rows = mysql_num_rows($qresult);
-					echo "<br><font color='#004080' face=helvetica size=-1>".$lane_num.": ".$lane_num_rows." records successfully synchronized";
-				}
-				else {
-					echo "<br><font color='#800000' face=helvetica size=-1>Unable to synchronize ".$lane_num."</font>";
-				}
-			}
-		}
-
-	}
-	}
-
-}
-$time = strftime("%m-%d-%y %I:%M %p", time());
-echo "<p> <p><font color='#004080' face=helvetica size=-1>last run: ".$time."</font>";
-
-
-
-}
-
-function synctable($table,$serveruser,$opDB,$lane) {
-//	openlog("is4c_connect", LOG_PID | LOG_PERROR, LOG_LOCAL0);
-
-	if ($_SESSION["lanePass"] == "") {
-		$lanepass = "";
-	}
-	else {
-		$lanepass = "-p".$_SESSION["lanePass"];
-	}
-
-	if ($_SESSION["mPass"] == "") {
-		$serverpass = "";
-	}
-	else {
-		$serverpass = "-p".$_SESSION["mPass"];
-	}
-
-	$sync =  "mysqldump -u ".$_SESSION["mUser"]." ".$serverpass." -t ".$opDB." ".$table
-		  ." | mysql -u ".$_SESSION["laneUser"]." ".$lanepass." -h ".$lane." ".$opDB." 2>&1";
-//	echo $sync;
-	$error = 0;
-	$output = "";
-	exec($sync, $aResult);
-	foreach ($aResult as $errormsg) {
-		if ($errormsg && strlen($errormsg) > 0) {
-			$output = $output."\n".$errormsg;
-			$error = 1;
-		}
-	}
-
-	if ($error == 1) {
-
-              	syslog(LOG_WARNING, "synctable($table) failed; rc: errormsg: '$output'");
-              	return 0;
-
 	} else {
-		return 1;
+		header("Location: index.php");
 	}
-
-}
-
-/*
-
-function synctable_old($table,$serveruser,$opDB,$lane) {
-	openlog("is4c_connect", LOG_PID | LOG_PERROR, LOG_LOCAL0);
-	exec('mysqldump -u '.$serveruser.' -t '.$opDB.' '.$table.' | mysql -h '.$lane.' '.$opDB.".$table." 2>&1", $result, $return_code);
-	foreach ($result as $v) {$output .= "$v\n";}
-	if ($return_code == 0) {
-
-		return 1;
+	
+	$html='<!DOCTYPE HTML>
+<html>
+	<head>
+		<link href="screen.css" media="screen" rel="stylesheet" type="text/css"/>
+		<title>Synchronization - '.$type.'</title>
+	</head>
+	<body>
+		<h1>Synchronize '.$type.'</h1>
+		<form action="./synchronize.php" method="post" name="synchronize">';
+	
+// TODO - Auto generate lanes from define.conf, for now, hardcode
+	$lanes=array(
+		array('Name'=>'Lane 01', 'IP'=>'10.10.10.115')
+	);
+	
+	foreach ($lanes as $lane) {
+		$html.='
+			<label>'.$lane['Name'].'</label>
+			<input checked name="lanes[]" type="checkbox" value="'.$lane['IP'].'"/>';
+	}
+	
+	$html.='
+			<input type="hidden" name="t" value="'.$name.'"/>
+			<input type="submit"/>
+		</form>
+		<p class="status">';
+	
+	$link=mysql_connect($_SESSION["mServer"], $_SESSION["mUser"], $_SESSION["mPass"]);
+	if ($link) {
+		$query='SELECT `synchronizationLog`.`datetime` FROM `is4c_log`.`synchronizationLog` WHERE `synchronizationLog`.`name`=\''.$name.'\' AND `synchronizationLog`.`status`=1 ORDER BY `synchronizationLog`.`datetime` DESC LIMIT 1';
+		$result=mysql_query($query, $link);
+		if ($result && mysql_num_rows($result)==1) {
+			$row=mysql_fetch_array($result);
+			$html.='Last synchronized @ '.$row['datetime'];
+			
+		} else {
+			$html.='Unable to query synchronization log.';
+		}
 	} else {
-		syslog(LOG_WARNING, "synctable($table) failed; rc: '$return_code', output: '$output'");
-		return 0;
+		$html.='Unable to connect to main server.';		
 	}
-}
-*/
+
+	$html.='</p>
+	</body>
+</html>';
+
+	print_r($html);
 ?>
